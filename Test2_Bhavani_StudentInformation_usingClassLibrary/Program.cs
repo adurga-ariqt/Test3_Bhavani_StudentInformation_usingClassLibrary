b35410464ffae03b255715777729377e53bab08f
adurga-ariqt <adurga@ariqt.com>
2022-11-20 12:14:20 +0530

    Add project files.

---
+++ Test2_Bhavani_StudentInformation_usingClassLibrary/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyClassLibraryTest3_UsingInterfacesAnd_Inheritace;
+
+Console.WriteLine("Hello, World!");
+Grade objGrade = new Grade();
+objGrade.GradeMethod();
+Console.ReadLine();
